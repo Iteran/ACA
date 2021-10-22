90f26595d11e1766becda7fbc7036076edd20169
Exiteras <tom.rogiers@gmail.com>
2021-10-22 12:21:26 +0200

    ajout post deployement

--- ACADB/obj/Debug/postdeploy.sql
+++ ACADB/obj/Debug/postdeploy.sql
@@ -10,4 +10,10 @@ Post-Deployment Script Template
 --------------------------------------------------------------------------------------
 */
 insert into BaseProducts (Name,Picture,Description,Quantity) values('TissuBleu','ahjzhrhtht','Tissu mais bleu',10)
+exec InsertUser 't@t','t'
+update Users set IsAdmin = 1 where Id = 1
+exec InsertCustomer 'Rog','Tom','t@t','LaRueOk','ACA','15151056521'
+
+Declare @date date = getdate()
+exec InsertOrder @date,1,200,0
 GO
